f706a2859c5da70357e564173e3100d459dd4795
Abagail Hudson <abagailanne@gmail.com>
2019-03-31 13:20:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
-}
+
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, {[key]: value})
